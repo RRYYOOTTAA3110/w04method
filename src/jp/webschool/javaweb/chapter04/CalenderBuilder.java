555f16f2bc1cf3aa2bfa77eb0a7a3cbbab471e1e
RRYYOOTTAA3110 <john0316shinkou@gmail.com>
2019-04-03 10:23:31 +0900

    カレンダーの作成１
    カレンダー表示の下準備を行いました。

--- src/jp/webschool/javaweb/chapter04/CalenderBuilder.java
+++ src/jp/webschool/javaweb/chapter04/CalenderBuilder.java
@@ -1,6 +1,7 @@
 package jp.webschool.javaweb.chapter04;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -25,16 +26,40 @@ public class CalenderBuilder extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
 		int year = Integer.parseInt(request.getParameter("year"));
 		int month = Integer.parseInt(request.getParameter("month"));
+
+		String title = year + "年" + month + "月";
+		String css = "/w04method/calender.css";
+		int[] calenderArray = buildCalenderArray(year, month -1);
+		StringBuilder calenderTable = buildCalenderTable(calenderArray);
+		StringBuilder html = buildHtml(title, css, calenderTable);
+
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println(html);
+
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
 		int year = Integer.parseInt(request.getParameter("year"));
 		int month = Integer.parseInt(request.getParameter("month"));
+
+		String title = year + "年" + month + "月";
+		String css = "/w04method/calender.css";
+		int[] calenderArray = buildCalenderArray(year, month -1);
+		StringBuilder calenderTable = buildCalenderTable(calenderArray);
+		StringBuilder html = buildHtml(title, css, calenderTable);
+
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println(html);
+
 	}
 
 }
